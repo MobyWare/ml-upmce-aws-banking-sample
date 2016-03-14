e3d2a3c545ba5f7930fb94cf39702951f20f0051
dickm <dickm@upmc.edu>
2016-03-14 12:07:18 -0400

    Updated script src to S3 and added bulk results

--- Banking.R
+++ Banking.R
@@ -1,29 +1,37 @@
-banking = read.csv("C:/Users/dickm/Documents/Projects/ML/Source/UPMC/AWS/bank/bank.csv")
-
 library(caTools)
-set.seed(144)
-spl = sample.split(banking$y, 0.75)
-train = subset(banking, spl == TRUE)
-test = subset(banking, spl == FALSE)
-
-modelLM = glm(y~., data=train, family=binomial)
-
-#What's important in the model
-summary(modelLM)
-
-predLM = predict(modelLM, type="response", newdata=test)
-
-table(test$y,predLM > 0.5)
-
 #install.packages("ROCR")
 library("ROCR")
 
+start.time = proc.time()
+# read data
+banking = read.csv("https://s3.amazonaws.com/aws.banking.sample/bank.csv")
+
+# split data
+set.seed(144)
+spl = sample.split(banking$y, 0.70)
+train = subset(banking, spl == TRUE)
+test = subset(banking, spl == FALSE)
+
+# Train model
+modelLM = glm(y~., data=train, family=binomial)
+
+
+# Evaluate Model
+predLM = predict(modelLM, type="response", newdata=test)
+
+#Threshold 0.5
+table(test$y,predLM > 0.5)
+
+#Threshold 0.88
+table(test$y,predLM > 0.88)
+
 ROCRpred = prediction(predLM, test$y)
 as.numeric(performance(ROCRpred, "auc")@y.values)
+proc.time() - start.time
 
 
-# Using more data
+# Inspect model
+summary(modelLM)
 
-bankingFull = read.csv("C:/Users/dickm/Documents/Projects/ML/Source/UPMC/AWS/bank/bank-full.csv")
-str(bankingFull)
+
 
